49aecf6c945c2c657c4c3f561598e4b94901ff0e
j4yu22 <jay.e.underwood@gmail.com>
2024-10-07 21:50:26 -0600

    worked on prove

--- PandC/student/lesson_03/prove/prove.py
+++ PandC/student/lesson_03/prove/prove.py
@@ -30,7 +30,7 @@ CPU_COUNT = mp.cpu_count() + 4
 
 # TODO Your final video needs to have 300 processed frames.
 # However, while you are testing your code, set this much lower!
-FRAME_COUNT = 20
+FRAME_COUNT = 300
 
 # RGB values for reference
 RED = 0
@@ -38,16 +38,7 @@ GREEN = 1
 BLUE = 2
 
 def create_new_frame(image_file, green_file, process_file):
-    """"
-    Creates a new image file from image_file and green_file.
-    
-    Parameters:
-        image_file (str):   The path including name of the image to place on the green screen.
-        green_file (str):   The path including name of the green screen image to process.
-        process_file (str): The path including name of the file to save the processed image to.
-    """
-
-    # this print() statement is there to help see which frame is being processed
+    """" Creates a new image file from image_file and green_file."""
     print(f'{process_file[-7:-4]}', end=',', flush=True)
 
     image_img = Image.open(image_file)
@@ -60,10 +51,29 @@ def create_new_frame(image_file, green_file, process_file):
     mask = (np_img[:, :, BLUE] < 120) & (np_img[:, :, GREEN] > 120) & (np_img[:, :, RED] < 120)
 
     # Create mask image
-    mask_img = Image.fromarray((mask*255).astype(np.uint8))
+    mask_img = Image.fromarray((mask * 255).astype(np.uint8))
 
     image_new = Image.composite(image_img, green_img, mask_img)
     image_new.save(process_file)
+
+
+def process_frames(frame_range, cores):
+    """ Process frames using the given number of CPU cores """
+    start_time = timeit.default_timer()
+    
+    # Define the list of frame numbers for processing
+    frames = list(frame_range)
+    
+    # Use multiprocessing pool to process frames in parallel
+    with mp.Pool(cores) as pool:
+        pool.starmap(
+            create_new_frame,
+            [(f'elephant/image{frame:03d}.png', f'green/image{frame:03d}.png', f'processed/image{frame:03d}.png') for frame in frames]
+        )
+    
+    total_time = timeit.default_timer() - start_time
+    print(f'\nTime To Process all images using {cores} cores = {total_time}')
+    return total_time
 
 
 def main():
@@ -73,28 +83,23 @@ def main():
     xaxis_cpus = []
     yaxis_times = []
 
-    # TODO Process all frames trying 1 cpu, then 2, then 3, ... to CPU_COUNT
+    # Process all frames trying 1 cpu, then 2, then 3, ... up to CPU_COUNT
+    for cores in range(1, CPU_COUNT + 1):
+        log.write(f'Processing with {cores} CPU cores...')
+        xaxis_cpus.append(cores)
 
-    # sample code: remove before submitting  >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-    # process one frame #10
-    image_number = 10
-
-    image_file = f'elephant/image{image_number:03d}.png'
-    green_file = f'green/image{image_number:03d}.png'
-    process_file = f'processed/image{image_number:03d}.png'
-
-    start_time = timeit.default_timer()
-    create_new_frame(image_file, green_file, process_file)
-    print(f'\nTime To Process all images = {timeit.default_timer() - start_time}')
-    # <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+        # Process frames and record time taken
+        time_taken = process_frames(range(1, FRAME_COUNT + 1), cores)
+        yaxis_times.append(time_taken)
+        log.write(f'Time for {FRAME_COUNT} frames using {cores} processes: {time_taken}')
 
     # Log the total time this took
     log.write(f'Total Time for ALL processing: {timeit.default_timer() - all_process_time}')
 
-    # create plot of results and also save it to a PNG file
+    # Create plot of results and also save it to a PNG file
     plt.plot(xaxis_cpus, yaxis_times, label=f'{FRAME_COUNT}')
     
-    plt.title('CPU Core yaxis_times VS CPUs')
+    plt.title('CPU Core times VS CPUs')
     plt.xlabel('CPU Cores')
     plt.ylabel('Seconds')
     plt.legend(loc='best')
