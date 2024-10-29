467c37e9ef6224d25bcadca4382483b9016e18b3
jayunderwood <jay.e.underwood@gmail.com>
2024-10-28 21:34:35 -0600

    finished prove

--- PandC/student/lesson_06/prove/prove.py
+++ PandC/student/lesson_06/prove/prove.py
@@ -15,7 +15,7 @@ import random
 import multiprocessing as mp
 import os.path
 import time
-import datetime
+from datetime import datetime
 import json
 
 # Include cse 251 common Python files - Don't change
@@ -36,7 +36,6 @@ WRAPPER_DELAY = 'wrapper-delay'
 
 class Bag():
     """ Bag of marbles - Don't change """
-
     def __init__(self):
         self.items = []
 
@@ -51,14 +50,13 @@ class Bag():
 
 
 class Gift():
-    """
-    Gift of a large marble and a bag of marbles - Don't change
-
+    """ 
+    Gift of a large marble and a bag of marbles - Don't change 
+    
     Parameters:
         large_marble (string): The name of the large marble for this gift.
         marbles (Bag): A completed bag of small marbles for this gift.
     """
-
     def __init__(self, large_marble, marbles):
         self.large_marble = large_marble
         self.marbles = marbles
@@ -72,118 +70,125 @@ class Gift():
 class Marble_Creator(mp.Process):
     """ This class "creates" marbles and sends them to the bagger """
 
-    colors = ('Gold', 'Orange Peel', 'Purple Plum', 'Blue', 'Neon Silver',
-        'Tuscan Brown', 'La Salle Green', 'Spanish Orange', 'Pale Goldenrod', 'Orange Soda',
-        'Maximum Purple', 'Neon Pink', 'Light Orchid', 'Russian Violet', 'Sheen Green',
-        'Isabelline', 'Ruby', 'Emerald', 'Middle Red Purple', 'Royal Orange',
-        'Dark Fuchsia', 'Slate Blue', 'Neon Dark Green', 'Sage', 'Pale Taupe', 'Silver Pink',
-        'Stop Red', 'Eerie Black', 'Indigo', 'Ivory', 'Granny Smith Apple',
-        'Maximum Blue', 'Pale Cerulean', 'Vegas Gold', 'Mulberry', 'Mango Tango',
-        'Fiery Rose', 'Mode Beige', 'Platinum', 'Lilac Luster', 'Duke Blue', 'Candy Pink',
-        'Maximum Violet', 'Spanish Carmine', 'Antique Brass', 'Pale Plum', 'Dark Moss Green',
-        'Mint Cream', 'Shandy', 'Cotton Candy', 'Beaver', 'Rose Quartz', 'Purple',
-        'Almond', 'Zomp', 'Middle Green Yellow', 'Auburn', 'Chinese Red', 'Cobalt Blue',
-        'Lumber', 'Honeydew', 'Icterine', 'Golden Yellow', 'Silver Chalice', 'Lavender Blue',
-        'Outrageous Orange', 'Spanish Pink', 'Liver Chestnut', 'Mimi Pink', 'Royal Red', 'Arylide Yellow',
-        'Rose Dust', 'Terra Cotta', 'Lemon Lime', 'Bistre Brown', 'Venetian Red', 'Brink Pink',
-        'Russian Green', 'Blue Bell', 'Green', 'Black Coral', 'Thulian Pink',
-        'Safety Yellow', 'White Smoke', 'Pastel Gray', 'Orange Soda', 'Lavender Purple',
-        'Brown', 'Gold', 'Blue-Green', 'Antique Bronze', 'Mint Green', 'Royal Blue',
-        'Light Orange', 'Pastel Blue', 'Middle Green')
+    colors = ('Gold', 'Orange Peel', 'Purple Plum', 'Blue', 'Neon Silver', 
+              'Tuscan Brown', 'La Salle Green', 'Spanish Orange', 'Pale Goldenrod', 
+              'Orange Soda', 'Maximum Purple', 'Neon Pink', 'Light Orchid', 
+              'Russian Violet', 'Sheen Green', 'Isabelline', 'Ruby', 'Emerald', 
+              'Middle Red Purple', 'Royal Orange', 'Dark Fuchsia', 'Slate Blue', 
+              'Neon Dark Green', 'Sage', 'Pale Taupe', 'Silver Pink', 'Stop Red', 
+              'Eerie Black', 'Indigo', 'Ivory', 'Granny Smith Apple', 'Maximum Blue', 
+              'Pale Cerulean', 'Vegas Gold', 'Mulberry', 'Mango Tango', 'Fiery Rose', 
+              'Mode Beige', 'Platinum', 'Lilac Luster', 'Duke Blue', 'Candy Pink', 
+              'Maximum Violet', 'Spanish Carmine', 'Antique Brass', 'Pale Plum', 
+              'Dark Moss Green', 'Mint Cream', 'Shandy', 'Cotton Candy', 'Beaver', 
+              'Rose Quartz', 'Purple', 'Almond', 'Zomp', 'Middle Green Yellow', 
+              'Auburn', 'Chinese Red', 'Cobalt Blue', 'Lumber', 'Honeydew', 
+              'Icterine', 'Golden Yellow', 'Silver Chalice', 'Lavender Blue', 
+              'Outrageous Orange', 'Spanish Pink', 'Liver Chestnut', 'Mimi Pink', 
+              'Royal Red', 'Arylide Yellow', 'Rose Dust', 'Terra Cotta', 'Lemon Lime', 
+              'Bistre Brown', 'Venetian Red', 'Brink Pink', 'Russian Green', 
+              'Blue Bell', 'Green', 'Black Coral', 'Thulian Pink', 'Safety Yellow', 
+              'White Smoke', 'Pastel Gray', 'Orange Soda', 'Lavender Purple', 
+              'Brown', 'Gold', 'Blue-Green', 'Antique Bronze', 'Mint Green', 
+              'Royal Blue', 'Light Orange', 'Pastel Blue', 'Middle Green')
 
-    def __init__(self, settings, pipe):
+    def __init__(self, settings, out_queue):
         mp.Process.__init__(self)
         self.marble_count = settings[MARBLE_COUNT]
         self.creator_delay = settings[CREATOR_DELAY]
-        self.pipe = pipe
+        self.out_queue = out_queue
 
     def run(self):
         for _ in range(self.marble_count):
-            marble = random.choice(self.colors)
-            self.pipe.send(marble)
+            marble = random.choice(Marble_Creator.colors)
+            self.out_queue.put(marble)
             time.sleep(self.creator_delay)
 
-        self.pipe.send(None)  # Signal the end
+        self.out_queue.put(None)  
 
 
 class Bagger(mp.Process):
-    """ Receives marbles from the marble creator, then sends bags to the assembler """
-
-    def __init__(self, settings, in_pipe, out_pipe):
+    def __init__(self, settings, in_queue, out_queue):
         mp.Process.__init__(self)
         self.bag_count = settings[NUMBER_OF_MARBLES_IN_A_BAG]
         self.bagger_delay = settings[BAGGER_DELAY]
-        self.in_pipe = in_pipe
-        self.out_pipe = out_pipe
+        self.in_queue = in_queue
+        self.out_queue = out_queue
 
     def run(self):
-        bag = Bag()
         while True:
-            marble = self.in_pipe.recv()
-            if marble is None:
-                if bag.get_size() > 0:
-                    self.out_pipe.send(bag)  # Send the last incomplete bag
-                self.out_pipe.send(None)  # Signal the end
-                break
+            bag = Bag()
+            for _ in range(self.bag_count):
+                marble = self.in_queue.get()
+                if marble is None:
+                    self.out_queue.put(None)  # Signal no more bags
+                    return
 
-            bag.add(marble)
-            if bag.get_size() == self.bag_count:
-                self.out_pipe.send(bag)
-                bag = Bag()
-                time.sleep(self.bagger_delay)
+                bag.add(marble)
+
+            self.out_queue.put(bag)
+            time.sleep(self.bagger_delay)
 
 
 class Assembler(mp.Process):
-    """ Takes bags and creates gifts, then sends gifts to the wrapper """
-
     marble_names = ('Lucky', 'Spinner', 'Sure Shot', 'Big Joe', 'Winner', '5-Star', 'Hercules', 'Apollo', 'Zeus')
 
-    def __init__(self, settings, in_pipe, out_pipe):
+    def __init__(self, settings, in_queue, out_queue):
         mp.Process.__init__(self)
         self.assembler_delay = settings[ASSEMBLER_DELAY]
-        self.in_pipe = in_pipe
-        self.out_pipe = out_pipe
+        self.in_queue = in_queue
+        self.out_queue = out_queue
 
     def run(self):
         while True:
-            bag = self.in_pipe.recv()
+            bag = self.in_queue.get()
             if bag is None:
-                self.out_pipe.send(None)  # Signal the end
-                break
+                self.out_queue.put(None)  # Signal no more gifts
+                return
 
-            large_marble = random.choice(self.marble_names)
+            large_marble = random.choice(Assembler.marble_names)
             gift = Gift(large_marble, bag)
-            self.out_pipe.send(gift)
+            self.out_queue.put(gift)
             time.sleep(self.assembler_delay)
 
 
 class Wrapper(mp.Process):
-    """ Takes gifts and wraps them by saving to a file """
-
-    def __init__(self, settings, in_pipe, gift_counter):
+    def __init__(self, settings, in_queue, gift_counter):
         mp.Process.__init__(self)
         self.wrapper_delay = settings[WRAPPER_DELAY]
-        self.in_pipe = in_pipe
+        self.in_queue = in_queue
         self.gift_counter = gift_counter
 
     def run(self):
         with open(BOXES_FILENAME, 'w') as file:
             while True:
-                gift = self.in_pipe.recv()
+                gift = self.in_queue.get()
                 if gift is None:
                     break
 
-                timestamp = datetime.datetime.now().time()
+                timestamp = datetime.now().time()
                 file.write(f'Created - {timestamp}: {gift}\n')
                 with self.gift_counter.get_lock():
                     self.gift_counter.value += 1
                 time.sleep(self.wrapper_delay)
 
 
+def display_final_boxes(filename, log):
+    """ Display the final boxes file to the log file - Don't change """
+    if os.path.exists(filename):
+        log.write(f'Contents of {filename}')
+        with open(filename) as boxes_file:
+            for line in boxes_file:
+                log.write(line.strip())
+    else:
+        log.write_error(f'The file {filename} doesn\'t exist.  No boxes were created.')
+
+
 def main():
     """ Main function """
 
     log = Log(show_terminal=True)
+
     log.start_timer()
 
     # Load settings file
@@ -194,50 +199,49 @@ def main():
 
     log.write(f'Marble count     = {settings[MARBLE_COUNT]}')
     log.write(f'Marble delay     = {settings[CREATOR_DELAY]}')
-    log.write(f'Marbles in a bag = {settings[NUMBER_OF_MARBLES_IN_A_BAG]}')
+    log.write(f'Marbles in a bag = {settings[NUMBER_OF_MARBLES_IN_A_BAG]}') 
     log.write(f'Bagger delay     = {settings[BAGGER_DELAY]}')
     log.write(f'Assembler delay  = {settings[ASSEMBLER_DELAY]}')
     log.write(f'Wrapper delay    = {settings[WRAPPER_DELAY]}')
 
-    # Create Pipes
-    creator_to_bagger, bagger_to_creator = mp.Pipe()
-    bagger_to_assembler, assembler_to_bagger = mp.Pipe()
-    assembler_to_wrapper, wrapper_to_assembler = mp.Pipe()
+    # Create Queues between creator -> bagger -> assembler -> wrapper
+    creator_bagger_queue = mp.Queue()
+    bagger_assembler_queue = mp.Queue()
+    assembler_wrapper_queue = mp.Queue()
 
-    # Create shared variable to count the number of gifts created
+    # Create variable to be used to count the number of gifts
     gift_counter = mp.Value('i', 0)
 
-    # Delete final boxes file
+    # delete final boxes file
     if os.path.exists(BOXES_FILENAME):
         os.remove(BOXES_FILENAME)
 
     log.write('Create the processes')
 
     # Create the processes
-    creator = Marble_Creator(settings, creator_to_bagger)
-    bagger = Bagger(settings, bagger_to_creator, bagger_to_assembler)
-    assembler = Assembler(settings, assembler_to_bagger, assembler_to_wrapper)
-    wrapper = Wrapper(settings, wrapper_to_assembler, gift_counter)
+    creator = Marble_Creator(settings, creator_bagger_queue)
+    bagger = Bagger(settings, creator_bagger_queue, bagger_assembler_queue)
+    assembler = Assembler(settings, bagger_assembler_queue, assembler_wrapper_queue)
+    wrapper = Wrapper(settings, assembler_wrapper_queue, gift_counter)
 
     log.write('Starting the processes')
-
-    # Start processes
+    
+    # Start the processes
     creator.start()
     bagger.start()
     assembler.start()
     wrapper.start()
 
     log.write('Waiting for processes to finish')
-
-    # Wait for processes to complete
+    
+    # Wait for processes to finish
     creator.join()
     bagger.join()
     assembler.join()
     wrapper.join()
 
     display_final_boxes(BOXES_FILENAME, log)
-
-    # Log the number of gifts created
+    
     log.write(f'Total gifts created: {gift_counter.value}')
     log.stop_timer(f'Total time')
 
--- PandC/student/lesson_07/team/team.py
+++ PandC/student/lesson_07/team/team.py
@@ -2,7 +2,7 @@
 Course: CSE 251 
 Lesson: L07 Team
 File:   team.py
-Author: <Add name here>
+Author: =Jay Underwood
 
 Purpose: Retrieve Star Wars details from a server.
 
@@ -17,3 +17,100 @@ Instructions:
    retrieve data from the Star Wars website. Each request for data must be a apply_async() call;
    this means 1 url = 1 apply_async call, 94 urls = 94 apply_async calls.
 """
+
+from datetime import datetime, timedelta
+import requests
+import json
+import threading
+
+# Include cse 251 common Python files
+# this import wasn't working on my desktop for some reason
+from cse251 import *
+
+# Const Values
+TOP_API_URL = 'http://127.0.0.1:8790'
+
+# Global Variables
+call_count = 0
+
+class APIThread(threading.Thread):
+    def __init__(self, url, section):
+        threading.Thread.__init__(self)
+        self.url = url
+        self.section = section
+        self.result = None
+
+    def run(self):
+        global call_count
+        response = requests.get(self.url)
+        call_count += 1
+        if response.status_code == 200:
+            data = response.json()
+            if isinstance(data, dict):
+                self.result = data.get("name", "Unknown")
+            else:
+                self.result = data
+        else:
+            self.result = "Unknown"
+
+
+def get_data_from_section(urls, section_name):
+    threads = []
+    results = []
+
+    for url in urls:
+        thread = APIThread(url, section_name)
+        threads.append(thread)
+        thread.start()
+
+    for thread in threads:
+        thread.join()
+        results.append(thread.result)
+
+    return sorted(results)
+
+
+def main():
+    global call_count
+    log = Log(show_terminal=True)
+    log.start_timer('Starting to retrieve data from the server')
+
+    response = requests.get(TOP_API_URL)
+    call_count += 1
+
+    if response.status_code == 200:
+        api_urls = response.json()
+    else:
+        print("Error retrieving top API URL")
+        return
+
+    film_url = api_urls['films'] + "6"
+    response = requests.get(film_url)
+    call_count += 1
+
+    if response.status_code == 200:
+        film_data = response.json()
+    else:
+        print("Error retrieving film 6 data")
+        return
+
+    print(f"\nTitle   : {film_data['title']}")
+    print(f"Director: {film_data['director']}")
+    print(f"Producer: {film_data['producer']}")
+    print(f"Released: {film_data['release_date']}\n")
+
+    sections = ['characters', 'planets', 'starships', 'vehicles', 'species']
+    for section in sections:
+        urls = film_data[section]
+        results = get_data_from_section(urls, section)
+
+        print(f"{section.capitalize()}: {len(results)}")
+        print(', '.join(results))
+        print()
+
+    log.stop_timer('Total Time To complete')
+    log.write(f'There were {call_count} calls to the server')
+
+
+if __name__ == "__main__":
+    main()
